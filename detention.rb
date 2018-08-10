b8501302e6aa6b91ef3aaf0df00dcad2472c926f
sthai1721 <sophia.thai@rutgers.edu>
2018-08-10 15:37:16 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,63 @@
 #Code your detention class here
+
+class Detention
+  
+  def initialize
+    @activity="collective punishment"
+    @students=[]
+  end
+ 
+ def activity
+    @activity
+ end
+
+  def location
+    @location
+  end
+  
+  def location=(room)
+    @location=room
+  end
+  
+  def time
+    @time
+  end
+  
+  def time=(clock)
+    @time=clock
+  end
+  
+  def grumpy_teacher
+    @grump_teacher
+  end
+  
+  def grumpy_teacher=(teacher)
+    @grump_teacher=teacher
+  end
+  
+  def students
+  @students
+  end
+  
+
+  def add_student(name)
+  @students.push(name)
+  end
+  
+  def remove_student(name)
+    x=@students.length
+    count=0
+    while count<=x
+    if  @students[count] == name
+      break
+    count+=1
+    end
+    end
+    @students.delete_at(count)
+  end
+  
+
+  
+ 
+  
+end
